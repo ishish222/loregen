2c9da51c1f2fc817f7b0fd4c2b9cf62c9c7f994c
ishish.io <ts@ishish.io>
2025-04-23 21:03:48 +0200

    Corrections in frontend

--- src/frontend/dashboard.py
+++ src/frontend/dashboard.py
@@ -1,5 +1,6 @@
 import gradio as gr
 import os
+import pandas as pd
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())
 
@@ -13,9 +14,40 @@ REDIRECT_URI_LOGOUT = os.environ['COGNITO_DOMAIN_REDIRECT_URI_LOGOUT']
 CODE_VERSION = os.environ['CODE_VERSION']
 
 
+def generate_global_history(conditions):
+    history_rows = []
+
+    for i in range(10):
+        history_rows.append({
+            "epoch": i,
+            "name": "test",
+            "description": "test"
+        })
+
+    history_df = pd.DataFrame(history_rows)
+
+    return history_df
+
+
 with gr.Blocks() as dashboard:
     with gr.Row():
         with gr.Column():
             account_label = gr.Label("account", label="Account")
         with gr.Column():
             logout_btn = gr.Button("Logout", link="/logout")
+    with gr.Row():
+        with gr.Tabs() as main_tabs:
+            with gr.TabItem("Global history"):
+                gh_conditions = gr.Textbox(label="Global history")
+                gh_button = gr.Button("Generate")
+                gh_output = gr.DataFrame(label="Global history")
+            with gr.TabItem("Country history"):
+                gr.DataFrame(label="Country history")
+            with gr.TabItem("City history"):
+                gr.DataFrame(label="City history")
+            with gr.TabItem("Family history"):
+                gr.DataFrame(label="Family history")
+            with gr.TabItem("Character history"):
+                gr.DataFrame(label="Character history")
+
+    gh_button.click(fn=generate_global_history, inputs=[gh_conditions], outputs=[gh_output])
